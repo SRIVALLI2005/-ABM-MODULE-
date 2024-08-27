7256dee90d123b97ed9c00f8c274a9a6f0127e35
SRIVALLI2005 <122994896+SRIVALLI2005@users.noreply.github.com>
2024-08-27 11:48:01 +0530

    Add files via upload

--- 16_bit_sub.asm
+++ 16_bit_sub.asm
@@ -1,13 +1,53 @@
 org 100h
-
-msg db 'Hello, this is a prompt message!$', 0
-
+num1 dw 5678h 
+num2 dw 2024h 
 start:
-mov ah, 09h       ; Function to display a string
-lea dx, msg       ; Load address of the message into DX
-int 21h           ; Call DOS interrupt to display the string
+ mov ax, num1 
+ sub ax, num2 
 
-mov ah, 4Ch       ; Terminate the program
-int 21h           ; End the program
-
-ret
+ mov bx, ax 
+ mov ah, bh 
+ shr ah, 4 
+ add ah, 30h
+ cmp ah, 39h 
+ jle print_high_nibble
+ add ah, 7 
+print_high_nibble:
+ mov dl, ah
+ mov ah, 02h 
+ int 21h
+ 
+ mov ah, bh
+ and ah, 0fh
+ add ah, 30h 
+ cmp ah, 39h 
+ jle print_low_nibble
+ add ah, 7 
+print_low_nibble:
+ mov dl, ah 
+ mov ah, 02h 
+ int 21h
+ 
+ mov ah, bl 
+ shr ah, 4 
+ add ah, 30h 
+ cmp ah, 39h 
+ jle print_high_nibble2
+ add ah, 7 
+print_high_nibble2:
+ mov dl, ah 
+ mov ah, 02h 
+ int 21h
+ 
+ mov ah, bl 
+ and ah, 0fh 
+ add ah, 30h 
+ cmp ah, 39h 
+ jle print_low_nibble2
+ add ah, 7 
+print_low_nibble2:
+ mov dl, ah 
+ mov ah, 02h 
+ int 21h
+ mov ah, 4ch 
+ int 21h
