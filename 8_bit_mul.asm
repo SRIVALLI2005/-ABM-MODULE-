121c4b6a89de071e9acaf7a521dad86a8478af99
SRIVALLI2005 <122994896+SRIVALLI2005@users.noreply.github.com>
2024-09-07 20:53:03 +0530

    Add files via upload

--- 8_bit_mul.asm
+++ 8_bit_mul.asm
@@ -1,34 +1,54 @@
 org 100h
-mov al,05h
-mov bl,02h
+mov al,20h
+mov bl,50h
 mul bl
+     mov bx, ax
+     mov ah, bh
+     and ah, 0F0h
+     shr ah, 4
+     add ah, 30h
+     cmp ah, 39h
+     jle first_digit
+     add ah, 7 
+first_digit:
+       mov dl, ah
+       mov ah, 02h
+       int 21h
+      mov ah, bh
+      and ah, 0Fh
+      add ah, 30h
+      cmp ah, 39h
+      jle sec_digit
+      add ah, 7
+      
+sec_digit:
+       mov dl, ah
+       mov ah, 02h
+       int 21h         
+     mov ah, bl
+     and ah, 0F0h
+     shr ah, 4
+     add ah, 30h
+     cmp ah, 39h
+     jle third_digit
+     add ah, 7
+     
+third_digit:
+       mov dl, ah
+       mov ah, 02h
+       int 21h
+      mov ah, bl
+      and ah, 0Fh
+      add ah, 30h
+      cmp ah, 39h
+      jle four_digit
+      add ah, 7
+      
+four_digit:
+       mov dl, ah
+       mov ah, 02h
+       int 21h         
+       mov ah, 4Ch
+       int 21h
 
-mov bl, al
-
-mov ah, al
-and ah, 0F0h
-shr ah,4
-add ah, 30h
-cmp ah, 39h
-jle print_first_digit
-add ah, 7  
-
-print_first_digit:
-mov dl,ah
-mov ah,02h
-int 21h
-
-mov ah, bl
-and ah, 0Fh
-add ah, 30h
-cmp ah, 39h 
-jle print_sec_digit
-add ah,7
-
-print_sec_digit:
-mov dl,ah
-mov ah, 02h
-int 21h
-
-mov ah,4Ch
-int 21h
+ret
