d8b6944cd7d006fe01fa8c1e85a5bc5db869b0d9
SRIVALLI2005 <122994896+SRIVALLI2005@users.noreply.github.com>
2024-09-07 20:52:41 +0530

    Add files via upload

--- 16_bit_mul.asm
+++ 16_bit_mul.asm
@@ -1,50 +1,110 @@
 org 100h
-mov ax,0050h 
-mov bx,0020h 
+s dw 5050h
+h dw 2020h
+
+mov ax, s
+mov bx, h
 mul bx
- mov bx, ax 
- mov ah, bh 
- shr ah, 4 
- add ah, 30h
- cmp ah, 39h 
- jle print_high_nibble
- add ah, 7 
-print_high_nibble:
- mov dl, ah
- mov ah, 02h 
- int 21h
- 
- mov ah, bh
- and ah, 0fh
- add ah, 30h 
- cmp ah, 39h 
- jle print_low_nibble
- add ah, 7 
-print_low_nibble:
- mov dl, ah 
- mov ah, 02h 
- int 21h
- 
- mov ah, bl 
- shr ah, 4 
- add ah, 30h 
- cmp ah, 39h 
- jle print_high_nibble2
- add ah, 7 
-print_high_nibble2:
- mov dl, ah 
- mov ah, 02h 
- int 21h
- 
- mov ah, bl 
- and ah, 0fh 
- add ah, 30h 
- cmp ah, 39h 
- jle print_low_nibble2
- add ah, 7 
-print_low_nibble2:
- mov dl, ah 
- mov ah, 02h 
- int 21h
- mov ah, 4ch 
- int 21h
+
+mov ah, dl
+and ah, 0F0h
+shr ah, 4
+add ah, 30h
+cmp ah, 39h
+jle first_digit
+add ah, 7 
+
+first_digit:
+mov dl, ah
+mov ah, 02h
+int 21h
+
+mov ah, dl
+and ah, 0Fh
+add ah, 30h
+cmp ah, 39h
+jle sec_digit
+add ah, 7 
+
+sec_digit:
+mov dl, ah
+mov ah, 02h
+int 21h
+
+mov ah, dh
+and ah, 0F0h
+shr ah, 4
+add ah, 30h
+cmp ah, 39h
+jle third_digit
+add ah, 7 
+
+third_digit:
+mov dl, ah
+mov ah, 02h
+int 21h
+
+mov ah, dh
+and ah, 0Fh
+add ah, 30h
+cmp ah, 39h
+jle four_digit
+add ah, 7 
+
+four_digit:
+mov dl, ah
+mov ah, 02h
+int 21h
+
+mov ah, al
+and ah, 0F0h
+shr ah, 4
+add ah, 30h
+cmp ah, 39h
+jle fifth_digit
+add ah, 7 
+
+fifth_digit:
+mov dl, ah
+mov ah, 02h
+int 21h
+
+mov ah, al
+and ah, 0Fh
+add ah, 30h
+cmp ah, 39h
+jle six_digit
+add ah, 7 
+
+six_digit:
+mov dl, ah
+mov ah, 02h
+int 21h
+
+mov ah, ah
+and ah, 0F0h
+shr ah, 4
+add ah, 30h
+cmp ah, 39h
+jle sev_digit
+add ah, 7 
+
+sev_digit:
+mov dl, ah
+mov ah, 02h
+int 21h
+
+mov ah, ah
+and ah, 0Fh
+add ah, 30h
+cmp ah, 39h
+jle ei_digit
+add ah, 7 
+
+ei_digit:
+mov dl, ah
+mov ah, 02h
+int 21h
+
+mov ah, 4Ch
+int 21h
